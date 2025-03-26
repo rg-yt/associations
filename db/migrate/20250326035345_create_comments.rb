769d0075056cc39522fc2d819fa73fafd48507c3
rg-yt <103154533+rg-yt@users.noreply.github.com>
2025-03-26 14:07:37 -0400

    Update migration to add reference to user on comments table

--- db/migrate/20250326035345_create_comments.rb
+++ db/migrate/20250326035345_create_comments.rb
@@ -3,6 +3,7 @@ class CreateComments < ActiveRecord::Migration[8.0]
     create_table :comments do |t|
       t.string :comment_content
       t.references :post, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
